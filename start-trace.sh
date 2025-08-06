cfb2ae7f3315d71552a6d0beaf56cd855bd4320f
Christian Schaf <christian.schaf@gmail.com>
2025-08-06 15:19:47 +0200

    Added readme, changed start-trace.sh

--- start-trace.sh
+++ start-trace.sh
@@ -1,1 +1,2 @@
-RUST_LOG="engineioxide=trace,socketioxide=trace,info" cargo watch -x run
+#RUST_LOG="engineioxide=trace,socketioxide=trace,info" cargo watch -x run
+RUST_LOG="engineioxide=trace" cargo watch -x run
